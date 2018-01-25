42b823a18f673aa40f53ed937a2a2bd369f92624
Jesse Elkharrat <jessenelk@gmail.com>
2018-01-25 00:02:51 -0500

    S6L74(lesson12)

--- SmackProj/Controller/CreateAccountVC.swift
+++ SmackProj/Controller/CreateAccountVC.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
+    
+    //outlets
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
+    
+    //variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,11 +43,21 @@ class CreateAccountVC: UIViewController {
             return
         }
         
+        guard let name = usernameTxt.text , usernameTxt.text != "" else {
+            return
+        }
+        
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
                 AuthService.instance.loginUser(email: email, pass: pass, completion: { (success) in
                     if success{
-                        print("Logged in!", AuthService.instance.authToken)
+                       // print("Logged in!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success{
+                                print(UserDataService.instance.avatarName, UserDataService.instance.name)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
                 //print("Rregistered User")
