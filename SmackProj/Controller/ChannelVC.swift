21fe619a49127a673e7b16c4b15573365089a778
Jesse Elkharrat <jessenelk@gmail.com>
2018-01-30 09:06:27 -0500

    S6L79(lesson16)

--- SmackProj/Controller/ChannelVC.swift
+++ SmackProj/Controller/ChannelVC.swift
@@ -12,6 +12,7 @@ class ChannelVC: UIViewController {
     
     //Outlets
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     
     //Takes us from CreateAccountVC to Channel
     //In main.storyboard, control drag from yellow box (UIViewController) to Orange Box all the way on the right and choose the function. Also need to give it an identifier in main.storyboard
@@ -23,10 +24,35 @@ class ChannelVC: UIViewController {
         
         self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
         
+        //The notification is being broadcasted in the createAccountVC and is being listened for here
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        if AuthService.instance.isLoggedIn{
+            //Show Profile Image
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+            
+        }else{
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+
+        }
     }
     
+    //selector for notification
+    @objc func userDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+            
+        } else {
+            loginBtn.setTitle("Log In", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
 }
--- SmackProj/Services/UserDataService.swift
+++ SmackProj/Services/UserDataService.swift
@@ -31,4 +31,59 @@ class  UserDataService {
     func setAvatarName(avatarName: String){
         self.avatarName = avatarName
     }
+    
+    //This functions take array of strings and breaks into different components to save RGB values into user data
+    func returnUIColor(components: String) -> UIColor {
+        
+        //interprets and converts NSstring object into number and string values
+        let scanner = Scanner(string: components)
+        
+        let skipped = CharacterSet(charactersIn: "[], ")
+        
+        let comma = CharacterSet(charactersIn: ",")
+        
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        //backup plan if upwrapping doesn't work
+        guard let rUnwrapped = r else {return defaultColor}
+        guard let gUnwrapped = g else {return defaultColor}
+        guard let bUnwrapped = b else {return defaultColor}
+        guard let aUnwrapped = a else {return defaultColor}
+
+        let rFloat = CGFloat(rUnwrapped.doubleValue)
+        let gFloat = CGFloat(gUnwrapped.doubleValue)
+        let bFloat = CGFloat(bUnwrapped.doubleValue)
+        let aFloat = CGFloat(aUnwrapped.doubleValue)
+
+        let newUIColor =  UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+        
+        return newUIColor
+    }
+    
+    func logOutUser(){
+        id = ""
+        name = ""
+        avatarName = ""
+        avatarColor = ""
+        email = ""
+        AuthService.instance.isLoggedIn = false
+        AuthService.instance.authToken = ""
+        AuthService.instance.userEmail = ""
+    }
+    
+    
+    
+    
+    
+    
+    
 }
