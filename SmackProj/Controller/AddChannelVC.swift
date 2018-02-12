db42749070786ff8e350bb56cb72dd383286c463
Jesse Elkharrat <jessenelk@gmail.com>
2018-02-12 07:57:13 -0500

    S6L83(lesson20)

---
+++ SmackProj/Controller/AddChannelVC.swift
@@ -0,0 +1,44 @@
+//
+//  AddChannelVC.swift
+//  SmackProj
+//
+//  Created by Nessin Elkharrat on 2/11/18.
+//  Copyright © 2018 practice. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpView()
+    }
+
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    func setUpView(){
+        let closedTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closedTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceholder])
+         chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceholder])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
